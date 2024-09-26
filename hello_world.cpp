5069c5515912de6560b959394a126f9ea35b1433
akarshhkh <akarshkhot@gmail.com>
2024-09-26 12:03:28 +0530

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+//akarsh khot
+//23070123012
+//Aim - Printing out Hello World
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello world";
+}
+
+/*
+OUTPUT:
+Hello world
+*/
